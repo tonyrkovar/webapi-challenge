b040256c3df40d299be516569908ac8240578c31
Tony <tonyrkovar@gmail.com>
2019-11-01 11:49:08 -0500

    Got my get action by project id and id working properly

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -2,18 +2,73 @@ const express = require('express')
 
 const db = require('../data/helpers/actionModel')
 
+const projectDB = require('../data/helpers/projectModel')
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    db.get()
+router.get('/:project_id/action/', validateProject, (req, res) => {
+    projectDB.get(req.params.project_id)
         .then(actions => {
-            res.status(200).json(actions)
+            if (actions) {
+                res.status(200).json(actions.actions)
+            } else {
+                res.status(400).json({ error: "There are no actions here" })
+            }
         })
         .catch(err => {
-            res.status(500).json({ errorMessage: `Bad request; ${err}` })
+            res.status(500).json({ error: "bad request" })
+        })
+
+})
+
+router.get('/:project_id/action/:id', validateProject, (req, res) => {
+    // db.get(req.params.id)
+    //     .then(action => {
+    //         res.status(200).json(action)
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json("Bad request")
+    //     })
+    projectDB.get(req.params.project_id)
+        .then(actions => {
+            if (actions) {
+                db.get(req.project.id)
+                    .then(action => {
+                        console.log(action.project_id)
+                        console.log(req.params.project_id)
+                        if (`${action.project_id}` === req.params.project_id) {
+                            res.status(200).json(action)
+                        } else {
+                            res.status(400).json({ error: "that action does not exist" })
+                        }
+                    })
+                    .catch(err => {
+                        res.status(500).json({ error: 'bad request' })
+                    })
+            } else {
+                res.status(401).json({ error: "There is no project here" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "bad request" })
         })
 })
 
+// router.update('/:project_id/action/:id', )
 
+function validateProject(req, res, next) {
+    projectDB.get(req.params.id)
+        .then(project => {
+            if (project) {
+                req.project = project
+                next()
+            } else {
+                res.status(400).json({ errorMessage: "That project does not exist" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ Error: 'bad request' })
+        })
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ server.get('/', (req, res) => {
     res.status(200).json("Get run succssefully")
 })
 server.use(express.json());
-server.use('/api/action', actionRouter)
+server.use('/api/project', actionRouter)
 server.use('/api/project', projectRouter)
 
 module.exports = server;
